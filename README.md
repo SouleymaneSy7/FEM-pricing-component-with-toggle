f3f4b2c89589dffbc038d3be9d80a569152e817d
Souleymane Sy <souleymanesyservices@gmail.com>
2023-07-28 00:40:42 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# Frontend Mentor - Pricing component with toggle
+
+![Design preview for the Pricing component with toggle coding challenge](./design/desktop-preview.jpg)
+
+## Welcome! 👋
+
+This is a solution to the [Pricing component with toggle challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/pricing-component-with-toggle-8vPwRMIC).
+
+[Frontend Mentor](https://www.frontendmentor.io) challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [The challenge](#the-challenge)
+- [Screenshot](#screenshot)
+- [Built with](#built-with)
+- [Author](#author)
+
+## The challenge
+
+The Challenge is to build out this pricing component and get it looking as close to the design as possible.
+
+Users should be able to:
+
+- View the optimal layout for the component depending on their device's screen size
+- Control the toggle with both their mouse/trackpad and their keyboard
+
+## Screenshot
+
+There is the Screenshot of my work!
+
+### Mobile Annually
+
+![Mobile Annually Screenshot](./preview/Mobile-Annually.png)
+
+### Mobile Monthly
+
+![Mobile Monthly Screenshot](./preview/Mobile-Monthly.png)
+
+### Desktop Annually
+
+![Desktop Annually Screenshot](./preview/Desktop-Annually.png)
+
+### Desktop Monthly
+
+![Desktop Monthly Screenshot](./preview/Desktop-Monthly.png)
+
+### Built with
+
+- Semantic HTML5 markup
+- SASS
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- Vite
+- NPM
+- Vue Js
+- Composition API
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## Author
+
+- GitHub - [Souleymane Sy](https://github.com/SouleymaneSy7)
+- Frontend Mentor - [@SouleymaneSy7](https://www.frontendmentor.io/profile/SouleymaneSy7)
+- Twitter - [@Souleymanesy43](https://twitter.com/Souleymanesy43)
---
+++ src/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <h1 class="main__title">Our Pricing</h1>
+
+  <!-- CheckBox  -->
+  <section class="checkbox-container">
+    <p class="sm__font">Annually</p>
+
+    <div
+      class="checkbox"
+      @click="priceToggle()"
+      :class="monthly === true ? 'active' : ''"
+    >
+      <div class="checkbox-toggle"></div>
+    </div>
+
+    <p class="sm__font">Monthly</p>
+  </section>
+
+  <!-- Card -->
+  <section class="card-container">
+    <article class="card" v-for="card in cards" :key="card.cardId">
+      <p class="base__font">{{ card.cardType }}</p>
+
+      <h2 class="price" v-if="monthly">
+        <span class="dollar-sign">&dollar;</span> {{ card.cardPriceMonthly }}
+      </h2>
+      <h2 class="price" v-else>
+        <span class="dollar-sign">&dollar;</span>{{ card.cardPriceAnnualy }}
+      </h2>
+
+      <div class="card__table">
+        <p class="base__font">{{ card.cardText1 }}</p>
+        <p class="base__font">{{ card.cardText2 }}</p>
+        <p class="base__font">{{ card.cardText3 }}</p>
+      </div>
+
+      <button type="button" class="btn">{{ card.cardBtnText }}</button>
+    </article>
+  </section>
+
+  <footer class="attribution">
+    <p>
+      Challenge by
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+        >Frontend Mentor</a
+      >
+    </p>
+
+    <p>
+      Code <span>&hearts;</span> by
+      <a href="https://github.com/SouleymaneSy7" target="_blank"
+        >Souleymane Sy</a
+      >
+    </p>
+  </footer>
+</template>
+
+<script setup>
+// Imports
+import { ref } from "vue";
+
+// Variables
+const monthly = ref(false);
+const cards = ref([
+  {
+    cardId: 1,
+    cardType: "Basic",
+    cardPriceAnnualy: 199.99,
+    cardPriceMonthly: 19.99,
+    cardText1: "500 GB Storage",
+    cardText2: "2 Users Allowed",
+    cardText3: "Send up to 3 GB",
+    cardBtnText: "Learn More",
+  },
+  {
+    cardId: 2,
+    cardType: "Professional",
+    cardPriceAnnualy: 249.99,
+    cardPriceMonthly: 24.99,
+    cardText1: "1 TB Storage",
+    cardText2: "5 Users Allowed",
+    cardText3: "Send up to 10 GB",
+    cardBtnText: "Learn More",
+  },
+  {
+    cardId: 3,
+    cardType: "Master",
+    cardPriceAnnualy: 399.99,
+    cardPriceMonthly: 39.99,
+    cardText1: "2 TB Storage",
+    cardText2: "10 Users Allowed",
+    cardText3: "Send up to 20 GB",
+    cardBtnText: "Learn More",
+  },
+]);
+
+// Functions
+const priceToggle = () => {
+  monthly.value = !monthly.value;
+};
+</script>
